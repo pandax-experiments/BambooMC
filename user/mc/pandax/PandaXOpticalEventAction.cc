7d16ea2b1dfd91282da343e7b18df944fc2f6b3f
Xun Chen <chenxun@sjtu.edu.cn>
2023-07-15 10:38:18 +0800

    reset random seed in PandaXOpticalEventAction

--- user/mc/pandax/PandaXOpticalEventAction.cc
+++ user/mc/pandax/PandaXOpticalEventAction.cc
@@ -1,7 +1,11 @@
 #include "PandaXOpticalEventAction.hh"
 #include "PandaXOpticalDataManager.hh"
 
+#include <CLHEP/Random/Random.h>
+
 #include <G4Event.hh>
+
+#include <chrono>
 
 PandaXOpticalEventAction::~PandaXOpticalEventAction() {
     G4cout << "PandaXOpticalEventAction destructed." << G4endl;
@@ -14,6 +18,13 @@ void PandaXOpticalEventAction::BeginOfEventAction(const G4Event *aEvent) {
     if (eventId % 100000 == 0) {
         G4cout << "Processing event " << eventId << "..." << G4endl;
     }
+    auto now = std::chrono::system_clock::now();
+    auto generator = CLHEP::HepRandom::getTheGenerator();
+    auto seed = std::chrono::duration_cast<std::chrono::nanoseconds>(
+                    now.time_since_epoch())
+                    .count() %
+                static_cast<int64_t>(1e16 * generator->flat());
+    CLHEP::HepRandom::setTheSeed(seed);
 }
 
 void PandaXOpticalEventAction::EndOfEventAction(const G4Event *aEvent) {
